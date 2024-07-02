8abb2e3008916e28455e7dd4f63cec53f175f6d8
lftobs <tobbytobs1@gmail.com>
2024-07-02 10:24:43 +0100

    task 1

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const image = document.querySelector("img");
 
 const currentDate = new Date();
 const dayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'long' });
-const currentTime = currentDate.toLocaleString('en-US', { timeStyle: 'short', timeZone: 'UTC' });
+const currentTimei = currentDate.toLocaleString('en-US', { timeStyle: 'short', timeZone: 'UTC' });
 
 window.addEventListener("load", (event) => {
   const { naturalWidth, naturalHeight, width, height } = image;
     day.textContent = `Current day of the week: ${dayOfWeek}`
-    time.textContent = `Current time: ${currentTime}`;
+    time.textContent = `Current time: ${currentTimei}`;
+    
 });
+
+setInterval(() => {
+  const currentDate = new Date();
+  const currentTime = currentDate.toLocaleString('en-US', { timeStyle: 'short', timeZone: 'UTC' });
+  time.textContent = `Current time: ${currentTime}`;
+}, 1000);
 
 
 
