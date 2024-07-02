7c2df5b735cb567e24e4f5467fc538b1dab22772
lftobs <tobbytobs1@gmail.com>
2024-07-02 10:00:30 +0100

    task 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const day = document.querySelector(".day");
+const time = document.querySelector(".time");
+const image = document.querySelector("img");
+
+const currentDate = new Date();
+const dayOfWeek = currentDate.toLocaleString('en-US', { weekday: 'long' });
+const currentTime = currentDate.toLocaleString('en-US', { timeStyle: 'short', timeZone: 'UTC' });
+
+window.addEventListener("load", (event) => {
+  const { naturalWidth, naturalHeight, width, height } = image;
+    day.textContent = `Current day of the week: ${dayOfWeek}`
+    time.textContent = `Current time: ${currentTime}`;
+});
+
+
+
